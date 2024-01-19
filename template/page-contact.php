8f764bc1a38493a4266b46422f37beb6033fb49f
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2024-01-19 10:24:17 +0100

    finalise template who & contact (scss and responsive)

--- template/page-contact.php
+++ template/page-contact.php
@@ -20,47 +20,61 @@ if (isset($_POST['submit']) && $_POST['firstname'] && $_POST['lastname'] !== '')
 ?>
 
 <main>
-    <div>
-        <form action="" method="post">
-            <h1>ME CONTACTER</h1>
-            <fieldset>
-                <label>Je suis intéressé.e par :</label>
-                <select name="interest">
-                    <option value="cours">Cours & ateliers</option>
-                    <option value="rituel">Rituel</option>
-                    <option value="accompagnement">Accompagnement psycho</option>
-                    <option value="autre">Autre...</option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <label>Nom</label>
-                <input type="text" name="lastname" placeholder="" value="" required="">
-            </fieldset>
-            <fieldset>
-                <label>Prénom</label>
-                <input type="text" name="firstname" placeholder="" value="" required="">
-            </fieldset>
-            <fieldset>
-                <label>Adresse mail</label>
-                <input type="email" name="email" placeholder="" value="" required="">
-            </fieldset>
-            <fieldset>
-                <label>Numéro téléphone</label>
-                <input type="tel" name="phone" placeholder="" value="" required="">
-            </fieldset>
-            <fieldset>
-                <label>Dites-m'en plus...</label>
-                <textarea name="additional_info" placeholder="" required=""></textarea>
-            </fieldset>
-            <p>Les données collectées seront seulement communiquées au destinataire suivant : Marion Cremona.</p>
-            <button type="submit" name="submit">Envoyer</button>
-        </form>
+    <div class="wrap">
+        <section class="contact">
+            <div class="form_contact">
+                <form action="" method="post">
+                    <h1>ME CONTACTER</h1>
+                    <fieldset>
+                        <div>
+                            <label>Je suis intéressé.e par :</label>
+                            <select name="interest">
+                                <option value="cours">Cours & ateliers</option>
+                                <option value="rituel">Rituel</option>
+                                <option value="accompagnement">Accompagnement psycho</option>
+                                <option value="autre">Autre...</option>
+                            </select>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label>Nom</label>
+                            <input type="text" name="lastname" placeholder="" value="" required="">
+                        </div>
+                        <div class="right">
+                            <label>Prénom</label>
+                            <input type="text" name="firstname" placeholder="" value="" required="">
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label>Adresse mail</label>
+                            <input type="email" name="email" placeholder="" value="" required="">
+                        </div>
+                        <div class="right">
+                            <label>Numéro téléphone</label>
+                            <input type="tel" name="phone" placeholder="" value="" required="">
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label>Dites-m'en plus...</label>
+                            <textarea name="additional_info" placeholder="" required=""></textarea>
+                        </div>
+                    </fieldset>
+                    <p>Les données collectées seront seulement communiquées au destinataire suivant : Marion Cremona.</p>
+                    <button type="submit" name="submit">ENVOYER</button>
+                </form>
+            </div>
+            <img class="splitter" loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/images/template/page-contact/splitter.svg" alt="Splitter">
+            <div class="contact_content">
+                <h2>Une question ? Un rendez-vous ?</h2>
+                <p><?php echo $explanation; ?></p>
+                <p>Une <a href="<?php echo $link ?>">Newsletter</a> par mois !</p>
+                <img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/images/template/page-contact/desktop-illustration.svg" alt="Illustration">
+            </div>
+        </section>
     </div>
-    <img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/images/template/page-contact/splitter.svg" alt="Splitter">
-    <h2>Une question ? Un rendez-vous ?</h2>
-    <p><?php echo $explanation; ?></p>
-    <p>Une <a href="<?php echo $link ?>">Newsletter</a> par mois ! </p>
-    <img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/images/template/page-contact/" alt="">
 </main>
 
 <?php
--- template/page-who.php
+++ template/page-who.php
@@ -10,57 +10,61 @@ get_header();
     $section_2_title = get_field('section_2_title');
     $section_3_title = get_field('section_3_title');
     ?>
-    <section>
-        <div>
-            <h1>ME DÉCOUVRIR ...</h1>
-            <h3><?php echo $section_1_title; ?></h3>
-            <div><?php echo $section_1_explanation; ?></div>
+    <div class="wrap">
+        <section class="bio">
+            <div class="bio_content">
+                <h1>ME DÉCOUVRIR ...</h1>
+                <h3><?php echo $section_1_title; ?></h3>
+                <div><?php echo $section_1_explanation; ?></div>
+            </div>
+            <img loading="lazy" src="<?php echo esc_url($section_1_photo['url']); ?>" alt="<?php echo esc_attr($section_1_photo['alt']); ?>">
+        </section>
+    </div>
+    <section class="values">
+        <div class="wrap">
+            <h2><?php echo $section_2_title; ?></h2>
+            <div class="parent">
+                <?php
+                if (have_rows('contents')) :
+                    while (have_rows('contents')) : the_row();
+                        $illustrated_text_title = get_sub_field('illustrated_text_title');
+                        $illustrated_text_illustration = get_sub_field('illustrated_text_illustration');
+                ?>
+                        <div>
+                            <img loading="lazy" src="<?php echo esc_url($illustrated_text_illustration['url']); ?>" alt="<?php echo esc_attr($illustrated_text_illustration['alt']); ?>">
+                            <h3><?php echo $illustrated_text_title; ?></h3>
+                        </div>
+                <?php
+                    endwhile;
+                endif;
+                ?>
+            </div>
         </div>
-        <img loading="lazy" src="<?php echo esc_url($section_1_photo['url']); ?>" alt="<?php echo esc_attr($section_1_photo['alt']); ?>">
     </section>
-    <section>
-        <h2><?php echo $section_2_title; ?></h2>
-        <div class="parent">
-            <?php
-            if (have_rows('contents')) :
-                while (have_rows('contents')) : the_row();
-                    $illustrated_text_title = get_sub_field('illustrated_text_title');
-                    $illustrated_text_explanation = get_sub_field('illustrated_text_explanation');
-                    $illustrated_text_illustration = get_sub_field('illustrated_text_illustration');
-            ?>
-                    <div>
-                        <h3><?php echo $illustrated_text_title; ?></h3>
-                        <div><?php echo $illustrated_text_explanation; ?></div>
-                        <img loading="lazy" src="<?php echo esc_url($illustrated_text_illustration['url']); ?>" alt="<?php echo esc_attr($illustrated_text_illustration['alt']); ?>">
-                    </div>
-            <?php
-                endwhile;
-            endif;
-            ?>
-        </div>
-    </section>
-    <section>
-        <h2><?php echo $section_3_title; ?></h2>
-        <div class="parent-2">
-            <?php
-            if (have_rows('curse')) :
-                $i = 0;
-                while (have_rows('curse')) : the_row();
-                    $text = get_sub_field('text');
-                    $class = $i % 2 == 0 ?"default":"alt";
-            ?>
-                    <div class="<?php echo $class; ?>">
-                        <div><?php echo $text; ?></div>
-                    </div>
-            <?php
-                    $i++;
-                endwhile;
-            else :
-            endif;
-            ?>
-        </div>
-        <img loading="lazy" src="<?php echo esc_url($illustrated_text_illustration['url']); ?>" alt="<?php echo esc_attr($illustrated_text_illustration['alt']); ?>">
-    </section>
+    <div class="wrap">
+        <section class="curse">
+            <h2><?php echo $section_3_title; ?></h2>
+            <div class="parent-2">
+                <?php
+                if (have_rows('curse')) :
+                    $i = 0;
+                    while (have_rows('curse')) : the_row();
+                        $text = get_sub_field('text');
+                        $class = $i % 2 == 0 ? "default" : "alt";
+                ?>
+                        <div class="<?php echo $class; ?>">
+                            <div><?php echo $text; ?></div>
+                        </div>
+                <?php
+                        $i++;
+                    endwhile;
+                else :
+                endif;
+                ?>
+            </div>
+            <img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/images/template/page-who/desktop-illustration.svg" alt="Illustration">
+        </section>
+    </div>
 </main>
 <?php
 get_footer();
